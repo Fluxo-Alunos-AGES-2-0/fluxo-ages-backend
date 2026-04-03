ec91fecf746e40c658a28ee5ae250f9e8bc3b0c5
Thales Xavier <thalescaceres@gmail.com>
2026-04-02 23:33:11 -0300

    chore: criando estrutura de pastas no backend

--- src/main/java/com/fluxo/infra/controller/HealthController.java
+++ src/main/java/com/fluxo/infra/controller/HealthController.java
@@ -1,4 +1,4 @@
-package com.fluxo.controller;
+package com.fluxo.infra.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
