49fab10c7589fcb52d553308650d9cdaac8840e9
ellenzv <ellenvitoriasm@gmail.com>
2026-04-16 18:54:33 -0300

    feat(US003-EndpointListagem): Endpoint de listagem de registros de ponto
    - Criado endpoint que retorna todos os registros de horas extracurriculares APROVADOS do aluno autenticado
    - Adicionada lógica do repository e service

--- src/main/java/com/fluxo/attendance/repository/AttendanceRepository.java
+++ src/main/java/com/fluxo/attendance/repository/AttendanceRepository.java
@@ -1,8 +1,10 @@
 package com.fluxo.attendance.repository;
 
 import com.fluxo.attendance.entity.Attendance;
+import com.fluxo.attendance.entity.AttendanceStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface AttendanceRepository extends JpaRepository<Attendance, Long> {
@@ -10,4 +12,5 @@ public interface AttendanceRepository extends JpaRepository<Attendance, Long> {
     boolean existsByStudentUserIdAndEndTimeIsNull(Long userId);
 
     Optional<Attendance> findFirstByStudentUserIdAndEndTimeIsNullOrderByStartTimeDesc(Long userId);
+    List<Attendance> findByStudentUserIdAndStatus(Long userId, AttendanceStatus status);
 }
