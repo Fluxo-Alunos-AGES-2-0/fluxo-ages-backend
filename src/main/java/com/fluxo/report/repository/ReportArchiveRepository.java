a5a2281af7b79889ade17e925739ff215f2f0bcc
pedrodalagnol <joao.rossatti@edu.pucs.br>
2026-05-11 15:26:31 -0300

    refactor: update ReportArchiveRepository to use ReportType enum for type parameters

--- src/main/java/com/fluxo/report/repository/ReportArchiveRepository.java
+++ src/main/java/com/fluxo/report/repository/ReportArchiveRepository.java
@@ -1,11 +1,14 @@
 package com.fluxo.report.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
 import com.fluxo.report.entity.ReportArchive;
+import com.fluxo.report.enums.ReportType; // <-- Não esqueça de importar o Enum!
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface ReportArchiveRepository extends JpaRepository<ReportArchive, Integer> {
-    Optional<ReportArchive> findByStudentUserIdAndType(Integer studentId, Integer type);
-    Optional<ReportArchive> findByStudentUserIdAndProjectIdAndType(Integer studentId, Integer projectId, Integer type);
+    Optional<ReportArchive> findByStudentUserIdAndType(Integer studentId, ReportType type);
+    Optional<ReportArchive> findByStudentUserIdAndProjectIdAndType(Integer studentId, Integer projectId, ReportType type);
 }
