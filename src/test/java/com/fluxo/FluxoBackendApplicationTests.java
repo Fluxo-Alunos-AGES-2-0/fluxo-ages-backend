c86091152a59da8b5a1c91ce2144a13c827ba341
Thales Xavier <thalescaceres@gmail.com>
2026-06-01 00:49:53 -0300

    fix(FluxoBackendApplicationTests): ativando contextloads para ver se pipeline roda normal

--- src/test/java/com/fluxo/FluxoBackendApplicationTests.java
+++ src/test/java/com/fluxo/FluxoBackendApplicationTests.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-@Disabled("Desativado porque o GitHub Actions não tem banco de dados configurado para subir o contexto") // <--- Adicione esta linha
 class FluxoBackendApplicationTests {
 
 	@Test
