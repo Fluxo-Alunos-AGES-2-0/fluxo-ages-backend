2395584b56739fe4f8d612644b000228d1dfe4bd
Felipe Seelend <felipeburgseelend@gmail.com>
2026-06-05 18:33:58 -0300

    Add async in method for send email

--- src/main/java/com/fluxo/FluxoBackendApplication.java
+++ src/main/java/com/fluxo/FluxoBackendApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder; // Não esqueça deste import!
 
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@EnableAsync
 @SpringBootApplication
 public class FluxoBackendApplication {
 
