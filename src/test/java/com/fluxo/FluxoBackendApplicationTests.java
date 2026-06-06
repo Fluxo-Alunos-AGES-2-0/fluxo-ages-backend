5f1cdfcb272f4b7d337e529dbabba0cb6674a45b
Thales Xavier <thalescaceres@gmail.com>
2026-06-05 21:23:36 -0300

    feat(tests): configurando propriedades do S3 testes

--- src/test/java/com/fluxo/FluxoBackendApplicationTests.java
+++ src/test/java/com/fluxo/FluxoBackendApplicationTests.java
@@ -1,10 +1,12 @@
 package com.fluxo;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+		"app.storage.s3.bucket=test-bucket",
+		"app.storage.s3.region=us-east-2"
+})
 class FluxoBackendApplicationTests {
 
 	@Test
